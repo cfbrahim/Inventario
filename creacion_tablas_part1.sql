e6c6f1cbe717c7ad44de39d1904ea4d0ba7e049f
cfbrahim <cfbrahimezzayer@torreroja.cat>
2025-03-18 18:47:27 +0100

    modificacions en la base de dades, hem afegit taula 'cargo' y taula 'personal' y lhem relacionat de 1 a N direccion cargo-->personal

--- creacion_tablas_part1.sql
+++ creacion_tablas_part1.sql
@@ -34,3 +34,25 @@ create table compra
 	constraint fk_com_producto foreign key(ref_producto) references producto(referencia)
     
 );
+
+
+create table cargo(
+id_cargo int auto_increment primary key ,
+cargo varchar(25) not null,
+permisos text
+); 
+
+
+create table personal (
+id_personal char(9)primary key ,
+nombre varchar(25) not null,
+apellido1 varchar(25) not null,
+apellido2 varchar(25) ,
+telefono varchar(15) ,
+email varchar(75) not null,
+fecha_contratacion date,
+cargo int,
+contraseña varchar(255)
+constraint fk_cargo foreign key (cargo) references cargo(id_cargo) on delete cascade
+
+);
